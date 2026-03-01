6e16b90292d8601327e79cdc0f8dd87bbec6eb1b
Marcus Silva <marcus.silva@forpeople.io>
2026-03-01 14:03:24 -0300

    feat: add icons with plugin svgr

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,73 @@
+import CopyIcon from './copy.svg?react';
+import CpuIcon from './cpu.svg?react';
+import DatabaseIcon from './database.svg?react';
+import DollarSignIcon from './dollar-sign.svg?react';
+import DropletIcon from './droplet.svg?react';
+import EyeIcon from './eye.svg?react';
+import FacebookIcon from './facebook.svg?react';
+import GiftIcon from './gift.svg?react';
+import GitBranchIcon from './git-branch.svg?react';
+import GitCommitIcon from './git-commit.svg?react';
+import GitMergeIcon from './git-merge.svg?react';
+import GitPullRequestIcon from './git-pull-request.svg?react';
+import GithubIcon from './github.svg?react';
+import GitlabIcon from './gitlab.svg?react';
+import GlobeIcon from './globe.svg?react';
+import GridIcon from './grid.svg?react';
+import HashIcon from './hash.svg?react';
+import HeartIcon from './heart.svg?react';
+import HomeIcon from './home.svg?react';
+import LayersIcon from './layers.svg?react';
+import LockIcon from './lock.svg?react';
+import MapPinIcon from './map-pin.svg?react';
+import MapIcon from './map.svg?react';
+import MicIcon from './mic.svg?react';
+import MonitorIcon from './monitor.svg?react';
+import PenToolIcon from './pen-tool.svg?react';
+import SearchIcon from './search.svg?react';
+import SettingsIcon from './settings.svg?react';
+import Share2Icon from './share-2.svg?react';
+import ShieldIcon from './shield.svg?react';
+import ShoppingCartIcon from './shopping-cart.svg?react';
+import ThumbsUpIcon from './thumbs-up.svg?react';
+import UserIcon from './user.svg?react';
+import XIcon from './x.svg?react';
+import ZapIcon from './zap.svg?react';
+
+export {
+  CopyIcon,
+  CpuIcon,
+  DatabaseIcon,
+  DollarSignIcon,
+  DropletIcon,
+  EyeIcon,
+  FacebookIcon,
+  GiftIcon,
+  GitBranchIcon,
+  GitCommitIcon,
+  GitMergeIcon,
+  GitPullRequestIcon,
+  GithubIcon,
+  GitlabIcon,
+  GlobeIcon,
+  GridIcon,
+  HashIcon,
+  HeartIcon,
+  HomeIcon,
+  LayersIcon,
+  LockIcon,
+  MapPinIcon,
+  MapIcon,
+  MicIcon,
+  MonitorIcon,
+  PenToolIcon,
+  SearchIcon,
+  SettingsIcon,
+  Share2Icon,
+  ShieldIcon,
+  ShoppingCartIcon,
+  ThumbsUpIcon,
+  UserIcon,
+  XIcon,
+  ZapIcon
+};
---
+++ src/svg.d.ts
@@ -0,0 +1,7 @@
+declare module '*.svg?react' {
+    import React from 'react';
+    const ReactComponent: React.FunctionComponent<
+        React.SVGProps<SVGSVGElement>
+    >;
+    export default ReactComponent;
+}
