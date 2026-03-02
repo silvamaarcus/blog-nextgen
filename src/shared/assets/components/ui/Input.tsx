820d07da1f33726a23a6dc75576d96df13ae7d66
Marcus Silva <marcus.silva@forpeople.io>
2026-03-02 20:40:47 -0300

    feat: add Input component

---
+++ src/shared/assets/components/ui/Input.tsx
@@ -0,0 +1,57 @@
+interface InputProps {
+  id?: string;
+  type?: string;
+  placeholder?: string;
+  errorMessage?: string;
+  icon?: React.ReactNode;
+  iconPosition?: 'left' | 'right';
+}
+
+const Input = ({
+  id,
+  type = 'text',
+  placeholder,
+  errorMessage,
+  icon,
+  iconPosition = 'left',
+  ...rest
+}: InputProps) => {
+  const getPositionClasses = () => {
+    if (!icon) return '';
+
+    if (iconPosition === 'left') {
+      return 'pl-16';
+    } else {
+      return 'pl-6';
+    }
+  };
+
+  return (
+    <div className="flex flex-col space-y-2 text-left">
+      <div className="flex w-full justify-end">
+        {errorMessage && (
+          <p className="text-brand-red text-base">{errorMessage}</p>
+        )}
+      </div>
+      <div className="relative">
+        <input
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          {...rest}
+          className={`bg-brand-black-light text-brand-white-light focus:ring-brand-blue h-14 w-full rounded focus:border-transparent focus:ring-2 focus:outline-none ${getPositionClasses()} ${errorMessage ? 'border-brand-red border' : 'border border-transparent'}`}
+        />
+
+        {icon && (
+          <div
+            className={`absolute top-4 h-6 w-6 [&>svg]:h-full [&>svg]:w-full ${iconPosition === 'left' ? 'left-6' : 'right-6'}`}
+          >
+            {icon}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Input;
