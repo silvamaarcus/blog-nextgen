ecaab05e38640deeb767b30740f1f3f71e6a6129
Marcus Silva <marcus.silva@forpeople.io>
2026-03-04 18:11:04 -0300

    feat: add className props

--- src/shared/assets/components/ui/TextTitle.tsx
+++ src/shared/assets/components/ui/TextTitle.tsx
@@ -1,44 +1,51 @@
 import { tv } from 'tailwind-variants';
 
 interface TextTitleProps {
-  text: string;
-  size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
-  color?: 'primary' | 'secondary';
+    children: string;
+    size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+    color?: 'primary' | 'secondary';
+    className?: string;
 }
 
-const TextTitle = ({ text, size = 'h1', color }: TextTitleProps) => {
-  const title = tv({
-    base: 'text-brand-blue font-semibold',
-    variants: {
-      color: {
-        primary: 'text-brand-blue',
-        secondary: 'text-brand-white-light',
-      },
-      size: {
-        h1: 'text-[56px] sm:text-8xl sm:leading-[0.9]',
-        h2: 'text-[40px] sm:text-[64px]',
-        h3: 'text-[32px] sm:text-5xl sm:leading-[1.1]',
-        h4: 'text-[28px] sm:text-4xl sm:leading-[1.2]',
-        h5: 'text-xl sm:text-2xl sm:leading-[1.3]',
-        h6: 'text-base sm:text-lg sm:leading-[1.4]',
-      },
-    },
-    defaultVariants: {
-      size: 'h1',
-      color: 'primary',
-    },
-  });
+const TextTitle = ({
+    children,
+    size = 'h1',
+    color,
+    className,
+}: TextTitleProps) => {
+    const title = tv({
+        base: 'text-brand-blue',
+        variants: {
+            color: {
+                primary: 'text-brand-blue',
+                secondary: 'text-brand-white-light',
+            },
+            size: {
+                h1: 'text-[56px] sm:text-8xl sm:leading-[0.9]',
+                h2: 'text-[40px] sm:text-[64px]',
+                h3: 'text-[32px] sm:text-5xl sm:leading-[1.1]',
+                h4: 'text-[28px] sm:text-4xl sm:leading-[1.2]',
+                h5: 'text-xl sm:text-2xl sm:leading-[1.3]',
+                h6: 'text-base sm:text-lg sm:leading-[1.4]',
+            },
+        },
+        defaultVariants: {
+            size: 'h1',
+            color: 'primary',
+        },
+    });
 
-  return (
-    <span
-      className={title({
-        size,
-        color,
-      })}
-    >
-      {text}
-    </span>
-  );
+    return (
+        <span
+            className={title({
+                size,
+                color,
+                className,
+            })}
+        >
+            {children}
+        </span>
+    );
 };
 
 export default TextTitle;
