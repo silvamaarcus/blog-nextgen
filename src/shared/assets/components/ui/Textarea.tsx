2b549e8f1ee101e1546a5bf11c1afd29c73ab55b
Marcus Silva <marcus.silva@forpeople.io>
2026-03-05 17:20:47 -0300

    fix: update Textarea component styles and remove default text

--- src/shared/assets/components/ui/Textarea.tsx
+++ src/shared/assets/components/ui/Textarea.tsx
@@ -8,12 +8,11 @@ const Textarea = ({ id, placeholder, className, ...rest }: TextareaProps) => {
     return (
         <textarea
             id={id}
-            className={`bg-brand-black-light text-brand-white-light focus:ring-brand-blue h-14 w-full rounded focus:border-transparent focus:ring-2 focus:outline-none ${className}`}
+            className={`bg-brand-black-light text-brand-white-light focus:ring-brand-blue h-50 w-full rounded p-6 focus:border-transparent focus:ring-2 focus:outline-none ${className}`}
             placeholder={placeholder}
+            cols={8}
             {...rest}
-        >
-            Textarea
-        </textarea>
+        />
     );
 };
 
